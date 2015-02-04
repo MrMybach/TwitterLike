ca8ef53b31ca18b105fc3aee3db82c64790f0f75
MrMybach <mybach@gmail.com>
2015-02-04 20:53:47 +0100

    root page changed and routes for retweets added

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :posts
+  resources :posts, path: 'tweets' do
+    member do
+      post :retweet
+    end
+  end
   resources :users
 
   resources :friendships, only: [:create]
 
-  root "users#index"
+  root "posts#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
