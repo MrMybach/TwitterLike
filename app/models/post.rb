1c9dc3dd6cbab983ac4ef6360c62e6aca4dcdbac
MrMybach <mybach@gmail.com>
2015-02-04 23:48:42 +0100

    Reordered fetching posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ActiveRecord::Base
+  default_scope { order(created_at: :desc) }
+
   belongs_to :user
   validates :text, presence: true, length: {minimum: 2, maximum: 140}
   belongs_to :parrent, class_name: 'Post'
