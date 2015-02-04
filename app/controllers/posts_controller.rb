818dea0688c30a37e085db9f606d028a680acb46
MrMybach <mybach@gmail.com>
2015-02-04 20:59:00 +0100

    fetching :posts moved to private method.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
+  before_action :fetch_posts, only: [:index, :create, :retweet, :destroy]
 
   def index
-    @posts = current_user.posts.page(params[:page]).per(5)
     @post = current_user.posts.new
   end
 
@@ -12,7 +12,19 @@ class PostsController < ApplicationController
       redirect_to posts_path
     else
       flash[:alert] = "Something went wrong. Please try again!"
-      render :new
+      render :index
+    end
+  end
+
+  def retweet
+    @post = Post.find(params[:id])
+    @retweet = Post.new(parrent_id: @post.id, text: @post.text, user_id: current_user.id)
+    if @retweet.save
+      flash[:success] = "Retweeted!"
+      redirect_to posts_path
+    else
+      flash[:alert] = "Something went wrong. Please try again!"
+      render :index
     end
   end
 
@@ -23,14 +35,18 @@ class PostsController < ApplicationController
   def destroy
     @post = current_user.posts.find(params[:id])
     @post.destroy
-    redirect_to root_path
+    render :index
     flash[:success] = "Deleted successfully!"
   end
 
   private
 
+  def fetch_posts
+    @posts = current_user.posts.page(params[:page]).per(5)
+  end
+
   def post_params
-    params.require(:post).permit(:title, :text)
+    params.require(:post).permit(:text)
   end
 
 end
